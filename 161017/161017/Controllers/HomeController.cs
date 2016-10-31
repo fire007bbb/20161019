92335f6239df4296e20ec5fd1d2b581ba948b7b6
stu <stu@qq.com>
2016-10-31 17:24:28 +0800

    URL表单传递

--- 161017/161017/Controllers/HomeController.cs
+++ 161017/161017/Controllers/HomeController.cs
@@ -14,16 +14,26 @@ namespace _161017.Controllers
         }
         public ActionResult welcome()
         {
+            var a = 100;
+            var sum = 0;
+            for (var i = 1; i <= a; i++)
+            {
+                sum += i;
+            }
+
+            ViewBag.sum = sum;
+            return View();
             return View();
         }
 
      
-        public ActionResult list()
+        public ActionResult list(int page = 10)
         {
             string[] data = new string[] {
                 "李克强：奋力开创东北全面振兴新局面","神十一与天宫对接成功 航天员“入宫”","这些作品影响习近平数十年"
             };
             ViewBag.data = data;
+            ViewBag.Page = page;
             return View();
         }
         public ActionResult About()
@@ -41,14 +51,14 @@ namespace _161017.Controllers
         }
         public ActionResult Add()
         {
-            ViewBag.Message = "Your contact page.";
+            
 
             return View();
         }
-        public ActionResult Save()
+        public ActionResult Save(string title, string content)
         {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.Title = title;
+            ViewBag.Content = content;
             return View();
         }
     }
