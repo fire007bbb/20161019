1a0f0e497d6d6561d799946925e122b3d02f0818
stu <stu@qq.com>
2016-11-14 15:05:08 +0800

    cookie认证demo

--- 161017/161017/Controllers/BlogController.cs
+++ 161017/161017/Controllers/BlogController.cs
@@ -32,9 +32,15 @@ namespace _161017.Controllers
 
         public ActionResult AddArticle()
         {
-            return View();
+            if (Request.Cookies["isauth"] != null && Request.Cookies["isauth"].Value == "true")
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("login", "cookiedemo");
+            }
         }
-
         //public ActionResult ArticleSave(string subject, string body)
         public ActionResult ArticleSave(BlogArticle model)
         {
